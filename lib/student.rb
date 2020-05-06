2af3ba9a059ac595a3b451732b4694f89c713bba
wei chen <weichen9083@gmail.com>
2020-05-06 01:17:52 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -13,67 +13,66 @@ def initialize(id = nil, name, grade)
   @grade = grade 
 end 
 
-def self.create_table
+def self.create_table 
   sql = <<-SQL
   CREATE TABLE IF NOT EXISTS students (
   id INTEGER PRIMARY KEY, 
   name TEXT,
-  grade TEXT)
+  grade TEXT
+  )
   SQL
   
-  DB[:conn].execute(sql)
+ DB[:conn].execute(sql)
   
 end 
 
-def self.drop_table 
-  sql = "DROP TABLE IF EXISTS students"
+
+def self.drop_table
+  sql = <<-SQL
+  DROP TABLE IF EXISTS students 
+  SQL
+  
   DB[:conn].execute(sql)
 end 
 
 def save 
-  
   if self.id 
     self.update
   else 
-sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
-    SQL
-
+  sql = <<-SQL
+  INSERT INTO students (name, grade) VALUES (?,?)
+  SQL
   
- DB[:conn].execute(sql, self.name, self.grade)
-
- @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
- end 
+  DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
 end 
-def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
-end
+end 
 
-def self.create (name, grade)
-    student = Student.new(name, grade)
-    student.save
-    student
-  end
+def update
+  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+  DB[:conn].execute(sql, self.name, self.grade, self.id)
+end 
+
+def self.create(name, grade)
+  new_student = Student.new(name, grade)
+  new_student.save 
+  new_student
+
+end 
+
+def self.new_from_db(row)
+  Student.new(row[0], row[1], row[2])
   
-  def self.new_from_db(row)
-   id = row[0]
-   name = row[1]
-   grade = row[2]
-   self.new(id, name, grade)
- end
+end 
 
 def self.find_by_name(name)
-  sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+  sql = <<-SQL
+  SELECT * FROM students WHERE name = ? 
+SQL
+
+DB[:conn].execute(sql,name).map do |row| 
+  self.new_from_db(row)
+end.first
 end 
-  
-  def update
-   sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-   DB[:conn].execute(sql, self.name, self.grade, self.id)
- end
-
-
 
 end
