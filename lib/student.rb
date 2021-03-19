cd96a78076da1396af03e7eab82082c902a44c6c
wei chen <weichen9083@gmail.com>
2021-03-19 03:20:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,10 +38,7 @@ class Student
   
   def self.new_from_db(row)
     new_student = Student.new(row[0],row[2],row[3])
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+ 
   end 
   
   def update
