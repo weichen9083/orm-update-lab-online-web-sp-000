ba594961f8a63661dfa29ab118eca623821b5f38
wei chen <weichen9083@gmail.com>
2021-03-19 03:11:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,10 +26,14 @@ class Student
   end 
   
   def save
+    if self.id 
+      self.update 
+    else 
     sql = "insert into students(name, grade) values (?,?)"
     DB[:conn].execute(sql,self.name,self.grade)
     
     self.id = DB[:conn].execute("select last_insert_rowid() from students")[0][0]
+   end 
     
   end 
     
