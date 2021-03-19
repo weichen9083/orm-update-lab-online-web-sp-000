d9d54d66d4ef36abfc7a14be4f0133ac2e6e570d
wei chen <weichen9083@gmail.com>
2021-03-19 03:06:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,13 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :id, :name, :grade
   
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade 
+  end 
 
 
 end
