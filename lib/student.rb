a8b9084e9df6aff87080a9676d26cd6b180b6586
wei chen <weichen9083@gmail.com>
2021-03-19 03:07:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,13 @@ class Student
     @name = name
     @grade = grade 
   end 
+  
+  def self.create_table
+    sql = "create table students(id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)"
+    
+    DB[:conn].execute(sql)
+    
+  end 
 
 
 end
